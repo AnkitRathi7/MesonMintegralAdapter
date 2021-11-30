1944c1b69a7c67aa9f52367e8512cb59fdce7ae8
Ankit Rathi <ankit.rathi@inmobi.com>
2021-11-30 09:19:51 +0530

    Initial

--- Package.swift
+++ Package.swift
@@ -14,15 +14,11 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "MesonSDK", url: "https://github.com/AnkitRathi7/MesonSDK", from: Version(1, 1, 0))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MesonMintegralAdapter",
-            dependencies: []),
-        .testTarget(
-            name: "MesonMintegralAdapterTests",
-            dependencies: ["MesonMintegralAdapter"]),
+        .binaryTarget(name: "MesonMintegralAdapter", url: "https://mesonsdk.s3.us-east-2.amazonaws.com/ios/plugins/MesonMintegralAdapter/MesonMintegralAdapter-7.0.3.0-beta1.zip", checksum: "d9e65265518327ae3af4a5a0007386ac88444aa07d6643165d29ec9ba0f36b10"),
     ]
 )
